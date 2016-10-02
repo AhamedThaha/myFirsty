7936041302ec215adcf5de77f3a88447c8b230c8
AhamedThaha <ahamed.thaha@outlook.com>
2016-10-02 22:52:43 +0200

    added app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: '10ba2ced-8a81-4cf4-83eb-12775049d17f', appPassword: 'S0h4oBn8dNAT3fDUx6rYFKF' }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
